2320e98546cc5c8a2bc94fd038164b91f07ab0c8
Coocoo1984 <yan.coocoo@qq.com>
2018-11-20 04:35:18 +0800

    commit

--- WebAPI_SQL/DAL/BL.cs
+++ WebAPI_SQL/DAL/BL.cs
@@ -87,7 +87,7 @@ namespace DAL
                     case 3: sb.Append("view_purchasing_plan_list_4_dept_need_modify"); break;
                 }
 
-                sb.Append(" WHERE department_id=");
+                sb.Append(" WHERE department_id = ");
                 sb.Append(departmentID);
                 try
                 {
@@ -106,7 +106,7 @@ namespace DAL
                 StringBuilder sb = new StringBuilder();
                 sb.Append("SELECT * FROM ");
                 sb.Append("view_purchasing_order_list_4_dept");
-                sb.Append(" WHERE department_id=");
+                sb.Append(" WHERE department_id = ");
                 sb.Append(departmentID);
                 try
                 {
@@ -125,7 +125,7 @@ namespace DAL
                 StringBuilder sb = new StringBuilder();
                 sb.Append("SELECT * FROM ");
                 sb.Append("view_purchasing_order_detail_list_4_dept");
-                sb.Append(" AND pod.purchasing_order_id=");
+                sb.Append(" AND pod.purchasing_order_id = ");
                 sb.Append(POid);
                 try
                 {
@@ -179,7 +179,7 @@ namespace DAL
                 StringBuilder sb = new StringBuilder();
                 sb.Append("SELECT * FROM ");
                 sb.Append("view_purchasing_plan_detail_goods_class_group_count");
-                sb.Append(" AND ppd.purchasing_plan_id=");
+                sb.Append(" AND ppd.purchasing_plan_id = ");
                 sb.Append(purchasingPlanId);
                 try
                 {
@@ -204,7 +204,7 @@ namespace DAL
                 StringBuilder sb = new StringBuilder();
                 sb.Append("SELECT * FROM ");
                 sb.Append("view_purchasing_plan_detail_list");
-                sb.Append(" AND ppd.purchasing_plan_id=");
+                sb.Append(" AND ppd.purchasing_plan_id = ");
                 sb.Append(purchasingPlanId);
                 try
                 {
@@ -229,10 +229,33 @@ namespace DAL
             if (bizTypeId > 1)
             {
                 StringBuilder sb = new StringBuilder();
-                sb.Append("SELECT * FROM ");
-                sb.Append("view_purchasing_plan_detail_list");
-                sb.Append(" AND ppd.purchasing_plan_id=");
+                sb.Append(@"
+SELECT
+    gc.name AS goods_class_name,
+	COUNT(qd.id) AS quote_detail_count
+FROM
+    quote AS q
+    LEFT JOIN quote_detail AS qd ON qd.quote_id = q.id
+    LEFT JOIN goods_class AS gc ON qd.goods_class_id = gc.id
+    LEFT JOIN biz_type AS bt ON q.biz_type_id = bt.id
+WHERE
+    q.disable = 0
+    AND qd.disable = 0
+                ");
+
+                sb.Append(" AND q.biz_type_id = ");
                 sb.Append(bizTypeId);
+              
+                if(startTime != null)
+                {
+                    sb.Append(" AND q.create_time > ");
+                    sb.Append(Convert.ToString(startTime));
+                }
+                if (endTime != null)
+                {
+                    sb.Append(" AND q.create_time < ");
+                    sb.Append(Convert.ToString(endTime));
+                }
                 try
                 {
                     result = DBHelper.ExecuteTable(sb.ToString());
@@ -243,7 +266,140 @@ namespace DAL
             return result;
         }
 
+        public static DataTable GetGoodsQuoteDetailVendorPriceRange(int bizTypeId, DateTime startTime, DateTime endTime)
+        {
+            DataTable result = null;
+            if (bizTypeId > 1)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(@"
+SELECT
+	qd.goods_class_id,
+	gc.name AS goods_class_name,
+	qd.goods_id,
+	g.name AS goods_name,
+	count( q.vendor_id ) AS vendor_count,
+	MIN( qd.unit_price ) AS min_unit_price,
+	MAX( qd.unit_price ) AS max_unit_price 
+FROM
+	quote AS q
+	LEFT JOIN quote_detail AS qd ON qd.quote_id = q.id
+	LEFT JOIN goods_class AS gc ON qd.goods_class_id = gc.id
+	LEFT JOIN vendor AS v ON q.vendor_id = v.id
+	LEFT JOIN biz_type AS bt ON q.biz_type_id = bt.id
+	LEFT JOIN goods AS g ON qd.goods_id = g.id 
+WHERE
+	q.disable = 0 
+	AND qd.disable = 0 
+                ");
 
+                sb.Append(" AND q.biz_type_id = ");
+                sb.Append(bizTypeId);
+
+                if (startTime != null)
+                {
+                    sb.Append(" AND q.create_time > ");
+                    sb.Append(Convert.ToString(startTime));
+                }
+                if (endTime != null)
+                {
+                    sb.Append(" AND q.create_time < ");
+                    sb.Append(Convert.ToString(endTime));
+                }
+                sb.Append(" GROUP BY goods_id");
+                try
+                {
+                    result = DBHelper.ExecuteTable(sb.ToString());
+                }
+                catch (Exception) { throw; }
+                finally { }
+            }
+            return result;
+        }
+
+        public static DataTable GetGoodsQuoteDetailVendorList(int bizTypeId, DateTime startTime, DateTime endTime, int goodsId)
+        {
+            DataTable result = null;
+            if (bizTypeId > 1 && goodsId > 1)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(@"
+SELECT
+	q.id AS quote_id,
+	q.create_time AS quote_create_time,
+	q.vendor_id,
+	v.name AS vendor_name,
+	qd.id AS quote_detail_id,
+	qd.goods_class_id,
+	qd.goods_id,
+	qd.unit_price,
+	qd.pre_unit_price,
+	g.name AS goods_name,
+	qd.unit_price - qd.pre_unit_price AS up_down 
+FROM
+	quote AS q
+	LEFT JOIN quote_detail AS qd ON qd.quote_id = q.id
+	LEFT JOIN goods_class AS gc ON qd.goods_class_id = gc.id
+	LEFT JOIN vendor AS v ON q.vendor_id = v.id
+	LEFT JOIN biz_type AS bt ON q.biz_type_id = bt.id
+	LEFT JOIN goods AS g ON qd.goods_id = g.id 
+WHERE
+	q.disable = 0 
+	AND qd.disable = 0 
+                ");
+
+                sb.Append(" AND q.biz_type_id = ");
+                sb.Append(bizTypeId);
+
+                if (startTime != null)
+                {
+                    sb.Append(" AND q.create_time > ");
+                    sb.Append(Convert.ToString(startTime));
+                }
+                if (endTime != null)
+                {
+                    sb.Append(" AND q.create_time < ");
+                    sb.Append(Convert.ToString(endTime));
+                }
+                sb.Append(" AND qd.goods_id = ");
+                sb.Append(goodsId);
+                sb.Append(" ORDER BY unit_price");
+                try
+                {
+                    result = DBHelper.ExecuteTable(sb.ToString());
+                }
+                catch (Exception) { throw; }
+                finally { }
+            }
+            return result;
+        }
+
+        public static DataTable GetQuoteListAll(DateTime startTime, DateTime endTime)
+        {
+            DataTable result = null;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("SELECT * FROM ");
+            sb.Append("view_quote_list_all");
+            if (startTime != null)
+            {
+                sb.Append(" AND q.create_time > ");
+                sb.Append(Convert.ToString(startTime));
+            }
+            if (endTime != null)
+            {
+                sb.Append(" AND q.create_time < ");
+                sb.Append(Convert.ToString(endTime));
+            }
+            try
+            {
+                result = DBHelper.ExecuteTable(sb.ToString());
+            }
+            catch (Exception) { throw; }
+            finally { }
+
+            return result;
+        }
 
     }
 }
---
+++ WebAPI_SQL/WebAPI_SQL/PagingHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_SQL
+{
+    public class PagingHelper
+    {
+        public static DataTable GetPagedTable(DataTable sourceDataTable, int PageIndex, int PageSize)//PageIndex表示第几页，PageSize表示每页的记录数
+        {
+            if (PageIndex == 0)
+                return sourceDataTable;//0页代表每页数据，直接返回
+
+            DataTable result = sourceDataTable.Copy();
+            result.Clear();//copy dt的框架
+
+            int rowbegin = (PageIndex - 1) * PageSize;
+            int rowend = PageIndex * PageSize;
+
+            if (rowbegin >= sourceDataTable.Rows.Count)
+                return result;//源数据记录数小于等于要显示的记录，直接返回dt
+
+            if (rowend > sourceDataTable.Rows.Count)
+                rowend = sourceDataTable.Rows.Count;
+            for (int i = rowbegin; i <= rowend - 1; i++)
+            {
+                DataRow newdr = result.NewRow();
+                DataRow dr = sourceDataTable.Rows[i];
+                foreach (DataColumn column in sourceDataTable.Columns)
+                {
+                    newdr[column.ColumnName] = dr[column.ColumnName];
+                }
+                result.Rows.Add(newdr);
+            }
+            return result;
+        }
+    }
+}
